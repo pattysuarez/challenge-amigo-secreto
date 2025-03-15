28a58f53c2f1780e67ac7288e3068a297e3b57a5
pattysuarezsantos@gmail.com <pattysuarezsantos@gmail.com>
2025-03-15 15:38:14 -0500

    actualiza el desarrollo del proyecto

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 # challenge-amigo-secreto
 
 <h1 align="center">CHALLENGE AMIGO SECRETO</h1>
+
+---
+
+En este desafío, desarrollarás una aplicación que permita a los usuarios ingresar nombres de amigos en una lista para luego realizar un sorteo aleatorio y determinar quién es el "amigo secreto".
+
+El usuario deberá agregar nombres mediante un campo de texto y un botón "Adicionar". Los nombres ingresados se mostrarán en una lista visible en la página, y al finalizar, un botón "Sortear Amigo" seleccionará uno de los nombres de forma aleatoria, mostrando el resultado en pantalla.
+
+Funcionalidades:
+Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto y lo agregarán a una lista visible al hacer clic en "Adicionar".
+
+Validar entrada: Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido.
+
+Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
+
+Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista y se mostrará en la página.
+
+---
+El propósito de este desafío es poner en práctica los conceptos adquiridos sobre lógica de programación. Para ello, hemos proporcionado el código HTML y CSS, lo que permitirá que el enfoque se centre completamente en aspectos fundamentales sobre lógica de programación como variables, condicionales, funciones y listas, entre otros.
+
+---
+Los archivos indicados en el proyecto trabajados fueron:
+
+- index.html: Formato de la pagina web.
+
+- app.js: código de ingreso de datos, inserción en la lista, sorteamiento mediante la formula matematica de los numeros de los arreglos que representaban la ubicación de la lista de los nombres.
+- style.css: estilo de la presetacion de las letras y graficas
+la carpeta assets: almacenamiento de las imagenes del archivo index.html
+
+---
+
+
+ 
+
--- app.js
+++ app.js
@@ -1,3 +1,56 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-//Claudia Patricia Suarez
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica 
+// para resolver el problema.
+//Claudia Patricia Suarez pattysuarezsantos@gmail.com
 //
+let amigos = [];
+
+function agregarAmigo() {
+    //ingresa el nombre del texto dede HTML
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value;
+
+    //console.log(amigo);
+    // valida si NO ha ingresado nombre
+    if(!nombreAmigo){
+        alert ("Por favor, inserte un nombre");
+        return;
+    }
+
+    //adicionar los nombres en un arreglo cada vez que se añade
+    amigos.push(nombreAmigo);
+    console.log(amigos);
+    inputAmigo.value = "";
+    inputAmigo.focus();
+    renderizarAmigos();
+}
+
+function renderizarAmigos(){
+    //mostrar la lista de nombres de amigos como una lista dinamica
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    for (let index = 0; index < amigos.length; index++) {
+        const element = amigos[index];
+    
+        let listaHTML = document.createElement("li");
+        listaHTML.textContent = element;
+        listaAmigos.appendChild(listaHTML);
+    }
+}
+
+function sortearAmigo() {
+    //sortear los nombres
+    if(amigos.length == 0){
+        alert("No hay amigos para sortear");
+        return;
+    }
+    let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `el amigo sorteado es: ${amigoSorteado}`;
+    
+    //limpia la lista de los nombres 
+    let limpiarLista = document.getElementById("listaAmigos");
+    limpiarLista.innerHTML="";
+
+}
+
